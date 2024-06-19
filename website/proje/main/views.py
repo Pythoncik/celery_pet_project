d6e87cf06aa4f0b31cab85087fec0c8e00f762fc
Maxim Labunckiy <maksimtemcenko18@gmail.com>
2024-06-19 18:49:09 +0300

    My first commit

---
+++ website/proje/main/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+from . import tasks
+
+
+def home(request):
+    tasks.download_a_cat.delay()
+    return HttpResponse('<h1>Загружаю кота...</h1>')
---
+++ website/proje/proje/celery.py
@@ -0,0 +1,21 @@
+import os
+
+
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proje.settings')
+
+
+app = Celery('proje')
+
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
